1a1fdc3b1fd64fff8c910b46f8405af90ef80893
Haider <janhaider040@gmail.com>
2023-05-29 21:15:25 -0400

    Model Trainer Done

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import dill
 import pandas as pd
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 
 ## this helps save the pickel file
@@ -20,5 +21,26 @@ def save_object(file_path, obj):
 
             dill.dump(obj, file_obj)
 
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+def evaluate_models(X_train, y_train, x_test, y_test, models):
+
+    try:
+        report={}
+
+        for i in range(len(list(models))):
+
+            model=list(models.values())[i]
+            model.fit(X_train, y_train) # Train Model
+
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(x_test)
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score= r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]]=test_model_score
+            return report
+        
     except Exception as e:
         raise CustomException(e,sys)
